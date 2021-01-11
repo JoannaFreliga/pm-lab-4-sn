24ddc7ad4d22c65a95680adba1b5148c293636db
Joanna Freliga <joanna.freliga@student.wat.edu.pl>
2021-01-11 10:37:23 +0100

    Wykonano zadanie 3.3.2 – tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+char tablica[] = "Joanna";
+int tabLen = 0;
+
+void lcdClear(void);
+
 
 void setup() {
     lcd.begin(16, 2);
-    lcd.print("Freliga");
+    lcd.print("Joanna Freliga");
+    tabLen = sizeof(tablica);
+    delay(2000);
 }
 
 void loop() {
-    lcd.setCursor(0, 1);
-    lcd.print(millis() / 1000);
-    delay(500);
-}
+    for (int i = 0; i < (18 - tabLen); i++){
+        lcdClear();
+        lcd.setCursor(i,0);
+        lcd.print(tablica);
+        delay(1000);
+    }
+
+    for (int i = (17 - tabLen); i < 0; i--) {
+        lcdClear();
+        lcd.setCursor(i,1);
+        lcd.print(tablica);
+        delay(1000);
+    }
+} 
